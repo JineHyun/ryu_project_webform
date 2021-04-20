2e44270b2c565a149ff1276051f1ef2babb84d10
JinHyun <rjs5967@naver.com>
2021-04-21 03:07:51 +0900

    ajax 추가

---
+++ ryu_project_webform/Views/Function/Function.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Function.aspx.cs" Inherits="ryu_project_webform.Views.Function.Function" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <script type="text/javascript">
+        $(function () {
+            $("#divMain").find("input[type=text]").keyup(function () {
+                $(this).val($(this).val().replace(/[^0-9]/g, ""));
+            });
+
+            var getVal = $("#<%=hidDictionary.ClientID %>").val();
+            $("#txtDictionary").val(getVal);
+
+            var obj = {
+                "data": [{
+                    "id": 1
+                    , "name": "홍길동"
+                }
+                    , {
+                    "id": 2
+                    , "name": "유관순"
+                }]
+            };
+
+            //ajax
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify(obj.data[0]),
+                url: "Function.aspx/functionAjax",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    var returnValue = JSON.parse(response.d);
+
+                }, beforeSend: function () {
+
+                }, complete: function () {
+
+                },
+                error: function (request, status, error) {
+                    alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+                }
+            });
+        });
+    </script>
+    <div id="divMain">
+        <h2>숫자만 입력</h2>
+        <input type="text" id="txtNumber" />
+        <h2>Dictionary 키 값 가져오기</h2>
+        <input type="text" id="txtDictionary" />
+        <h2>ajax</h2>
+        <input type="text" id="txtAjax" />
+        <asp:HiddenField ID="hidDictionary" runat="server"/>
+    </div>
+</asp:Content>
---
+++ ryu_project_webform/Views/Function/Function.aspx.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ryu_project_webform.Views.Function
+{
+    public partial class Function : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            functionDictionary();
+        }
+
+        private void InitializeControls()
+        {
+        }
+
+        /// <summary>
+        /// 기능모음
+        /// </summary>
+        public void functionCollection()
+        {
+        }
+
+        /// <summary>
+        /// Dictionary 키 값 가져오기
+        /// </summary>
+        /// <param name="keyName"></param>
+        /// <param name="getDic"></param>
+        /// <returns></returns>
+        public string getDictionaryValue(string keyName, Dictionary<string, string> getDic)
+        {
+            string result = string.Empty;
+            
+            foreach (KeyValuePair<String, String> item in getDic)
+            {
+                if (keyName.Contains(item.Key))
+                {
+                    result = item.Value.ToString();
+                }
+            }
+
+            return result;
+        }
+
+        public void functionDictionary()
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            dic.Add("id", "1");
+            dic.Add("name", "홍길동");
+
+            //키 등록 여부
+            if (!dic.ContainsKey("address"))
+            {
+                //Dictionary는 중복키X
+                dic.Add("address", "서울시 서대문구 미근동");
+            }
+
+            hidDictionary.Value = getDictionaryValue("address", dic);
+        }
+
+        [WebMethod]
+        public static string functionAjax(int id, string name)
+        {
+            string result = string.Empty;
+            
+            DataTable dt = new DataTable();
+            dt.TableName = "example";
+            dt.Columns.Add("id", typeof(int));
+            dt.Columns.Add("name", typeof(string));
+            DataRow dr = null;
+
+            dr = dt.NewRow();
+            dr["id"] = 1;
+            dr["name"] = "홍길동";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr["id"] = 2;
+            dr["name"] = "유관순";
+            dt.Rows.Add(dr);
+
+            result = JsonConvert.SerializeObject(dt);
+
+            return result;
+        }
+    }
+}
